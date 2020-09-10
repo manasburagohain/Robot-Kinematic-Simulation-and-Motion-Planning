9d0c6a59555a426e0bb1df29004e6fc65b4f9701
CI Grader <zhezhou@umich.edu>
2020-09-10 17:21:40 -0400

    Update CI grader check file at time: 10-09-2020-17-19-44

---
+++ grade.js
@@ -0,0 +1,347 @@
+grade = 
+{
+  "manasjb": {
+    "name": "Buragohain, Manas", 
+    "unique_name": "manasjb", 
+    "email": "manasjb@umich.edu", 
+    "course": "ROB 511 F20", 
+    "repository": "git@github.com:manasjyoti97/autorob.git", 
+    "assignments": {
+      "Honor": {
+        "status": "CHECK", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "PathPlan_Heap": {
+        "status": "PENDING", 
+        "test results": {
+          "Test 8": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when extract\nError when processing: 3037 1194 8211 9533 5749 7023 2666 9416 6186 6378 9143 9542 3732 6818 5883 447 1639 256 2392 9593 \n"
+          }, 
+          "Test 9": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when extract\nError when processing: 9939 5151 7422 9581 2790 3745 2168 4492 6273 6875 1691 3614 1206 654 7860 8107 3072 8076 6959 9689 \n"
+          }, 
+          "Test 2": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when extract\nError when processing: 1297 4426 5848 8800 7796 3267 5975 772 4664 9076 7199 8970 218 2694 6770 8262 8600 8155 8044 5028 \n"
+          }, 
+          "Test 3": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when extract\nError when processing: 4346 5012 8882 207 6994 3185 8003 3547 6942 9076 9277 7768 104 5219 5181 3276 9608 1762 6550 8843 \n"
+          }, 
+          "Test 0": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when extract\nError when processing: 5779 3963 5491 269 9105 9243 6308 4864 778 9581 6638 191 4278 563 827 7150 6564 1699 9990 1047 \n"
+          }, 
+          "Test 1": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when extract\nError when processing: 1038 6620 1737 1252 2121 6540 533 6914 4138 9327 5132 83 9475 158 6255 7652 7370 6658 6122 7803 \n"
+          }, 
+          "Test 6": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when extract\nError when processing: 3059 8251 9357 5919 7731 5780 3221 5505 5967 3082 8301 1093 5513 1185 1812 2108 2455 9610 7555 4344 \n"
+          }, 
+          "Test 7": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when extract\nError when processing: 8499 3591 1298 8890 7405 7365 3344 9933 8884 772 4036 2831 3632 6124 2707 5033 7459 8882 7462 2559 \n"
+          }, 
+          "Test 4": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when extract\nError when processing: 9476 1378 7336 5335 5958 8735 9707 4640 912 5503 6661 6241 7436 2554 2138 5109 7505 6493 865 6138 \n"
+          }, 
+          "Test 5": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR] wrong element when extract\nError when processing: 3167 7257 9037 9318 4491 7495 8623 8599 7639 7335 5762 5459 9595 9595 9090 5446 8655 2182 7703 8785 \n"
+          }
+        }, 
+        "comments": [
+          "0 out of 10 test cases passed"
+        ]
+      }, 
+      "PathPlan_AStar": {
+        "status": "PENDING", 
+        "test results": {
+          "Test 2": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 3": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 0": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 1": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 4": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }
+        }, 
+        "comments": [
+          "0 out of 5 test cases passed"
+        ]
+      }, 
+      "PathPlan_DFS": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "PathPlan_BFS": {
+        "status": "PENDING", 
+        "test results": {
+          "Test 2": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 3": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 0": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 1": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 4": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }
+        }, 
+        "comments": [
+          "0 out of 5 test cases passed"
+        ]
+      }, 
+      "PathPlan_BestFirst": {
+        "status": "PENDING", 
+        "test results": {
+          "Test 2": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 3": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 0": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 1": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }, 
+          "Test 4": {
+            "Status": "Failed", 
+            "Stderr": "[ERROR]: Search time out\n"
+          }
+        }, 
+        "comments": [
+          "0 out of 5 test cases passed"
+        ]
+      }, 
+      "Pendularm_Euler": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pendularm_VelocityVerlet": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pendularm_PID": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pendularm_Verlet": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pendularm_RK4": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pendularm_Double": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_MatrixRoutines": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_Transforms": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_JointRendering": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_BaseOffset": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FK_NewRobot": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_Quaternion": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_BaseControl": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_SetpointControl": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_FSM": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_JointLimits": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_Prismatic": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "FSMDance_Rosbridge": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "IK_Jacobian": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "IK_JTranspose": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "IK_JPseudoInverse": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "IK_EulerAngles": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "MotionPlan_Collision": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "MotionPlan_2DRRTConnect": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "MotionPlan_CSpaceRRTConnect": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "MotionPlan_RRTStar": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pitch": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Pitch_Advanced": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }, 
+      "Advanced": {
+        "status": "PENDING", 
+        "comments": [
+          ""
+        ]
+      }
+    }, 
+    "quizzes": [
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }, 
+      {
+        "status": "PENDING"
+      }
+    ]
+  }
+}
