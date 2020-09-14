6198e2876dba27882c882061153ef8cf1ea6aae0
CI Grader <zhezhou@umich.edu>
2020-09-14 00:59:56 -0400

    Update grades at time: 14-09-2020-00-56-33

--- grade.js
+++ grade.js
@@ -14,79 +14,79 @@ grade =
         ]
       }, 
       "PathPlan_Heap": {
-        "status": "PENDING", 
+        "status": "PASS", 
         "test results": {
           "Test 8": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when extract\nError when processing: 1484 7877 3679 4294 5081 3952 4834 4245 4872 8396 2903 806 2134 8721 8296 9330 1686 7551 4140 8947 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 9": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when extract\nError when processing: 1062 7972 6171 6375 6384 1384 7204 9969 7589 7456 4308 4340 5626 1023 1110 1833 5405 2046 1453 8026 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 2": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when extract\nError when processing: 9480 6607 7249 8271 1072 3583 4470 2902 8235 2814 6962 1514 2242 4867 6774 6965 4924 3549 7430 9743 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 3": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when extract\nError when processing: 7323 6709 384 4246 3988 7832 8665 5156 1524 6979 7784 3409 9753 5772 5748 239 9598 8010 4951 9524 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 0": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when extract\nError when processing: 1324 2311 5143 6193 3691 1658 9425 8165 9903 7528 714 8452 1903 1828 8162 8211 5623 8322 4474 8891 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 1": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when extract\nError when processing: 6940 4682 6807 5002 3069 6314 5261 9555 3153 2378 9511 158 2693 1917 6362 8894 3609 1795 7011 9930 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 6": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when extract\nError when processing: 9374 484 1618 992 8353 6800 9792 5849 3601 5572 2312 2528 6434 2181 6765 3650 2102 3471 9891 640 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 7": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when extract\nError when processing: 1821 8699 7550 4210 7543 9399 7489 387 6454 8884 9341 6215 2280 5871 6323 3929 9724 3232 4637 2085 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 4": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when extract\nError when processing: 7664 8730 1852 8048 8712 2308 5801 1161 5295 6266 6146 6252 4445 2412 4195 2726 1358 5990 4856 4723 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 5": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR] wrong element when extract\nError when processing: 907 2803 785 6198 3985 7708 14 915 1640 7533 6952 5998 344 1338 1475 6871 7632 1763 4610 5042 \n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }
         }, 
         "comments": [
-          "0 out of 10 test cases passed"
+          "10 out of 10 test cases passed"
         ]
       }, 
       "PathPlan_AStar": {
-        "status": "PENDING", 
+        "status": "PASS", 
         "test results": {
           "Test 2": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 3": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 0": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 1": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 4": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }
         }, 
         "comments": [
-          "0 out of 5 test cases passed"
+          "5 out of 5 test cases passed"
         ]
       }, 
       "PathPlan_DFS": {
@@ -96,59 +96,59 @@ grade =
         ]
       }, 
       "PathPlan_BFS": {
-        "status": "PENDING", 
+        "status": "PASS", 
         "test results": {
           "Test 2": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 3": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 0": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 1": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 4": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }
         }, 
         "comments": [
-          "0 out of 5 test cases passed"
+          "5 out of 5 test cases passed"
         ]
       }, 
       "PathPlan_BestFirst": {
         "status": "PENDING", 
         "test results": {
           "Test 2": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 3": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 0": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }, 
           "Test 1": {
             "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Stderr": "[ERROR]: Wrong result\n"
           }, 
           "Test 4": {
-            "Status": "Failed", 
-            "Stderr": "[ERROR]: Search time out\n"
+            "Status": "Succeed", 
+            "Stderr": "N/A"
           }
         }, 
         "comments": [
-          "0 out of 5 test cases passed"
+          "4 out of 5 test cases passed"
         ]
       }, 
       "Pendularm_Euler": {
